43d966d2f5ed87dd62bfba1f733339d75eb1febf
davi018 <31260474+davi018@users.noreply.github.com>
2017-08-23 09:44:13 +1000

    Add files via upload

---
+++ SocialMediaSite/SMIndex.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Home</title>
+        <div id="Container">
+          <header>
+            <nav>
+                <ul>
+                  <li><a href="Overview.php">Overview</a></li>
+                  <li><a href="Players.php">Impact on Players</a></li>
+                  <li><a href="Viewing.php">Changes in Viewership</a></li>
+                  <li><a href="Copyright.php">Copyright Issues</a></li>
+                  <li><a href="SMBibliography.php">Bibliography</a></li>
+                </ul>
+              </nav>
+          </header>
+          <div class="Main">
+          </div>
+      </div>
+    </body>
+</html>
